d9e5faa43eb62717a767c768bda8472f404d5e03
Dmytro DEmontazz <demontazz@aether-project.games>
2020-12-06 14:33:55 +0000

    Update CI

--- .space.kts
+++ .space.kts
@@ -4,15 +4,15 @@
 * For more info, refer to https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build and deploy web") {
-    container("cirrusci/flutter") {
+job("Build and deploy backend for web") {
+    container("gradle:jdk8") {
         env["IP"] = Params("web_backend_ip")
         env["DIR"] = Params("web_backend_dir")
         env["STARTKEY"] = Secrets("web_backend_key_1")
         env["ENDKEY"] = Secrets("web_backend_key_2")
         shellScript {
             content = """
-            	./gradlew war
+            	gradle war
                 touch key.pem
                 echo ${"$"}STARTKEY ${"$"}ENDKEY | sed 's/ /\n/g;w key.pem'  > /dev/null 2>&1
                 sed -i '1s/^/-----BEGIN OPENSSH PRIVATE KEY-----\n/' key.pem
